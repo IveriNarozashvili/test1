6f75700d8ee187f5fcbab48bdf3183712a32546d
IveriNarozashvili <geobmw91>
2020-04-11 19:51:20 +0300

    add Car class

---
+++ src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+
+    private String model;
+
+    public  Car;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                '}';
+    }
+}
